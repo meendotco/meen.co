fa6267af1bbe5182637602ee8213644c09bfbbd2
abbmarand <markusndrssn@gmail.com>
2025-04-11 17:54:14 +0200

    bugfixes with leeds and only send to slack

--- src/lib/server/mail/index.ts
+++ src/lib/server/mail/index.ts
@@ -15,7 +15,7 @@ async function notifyAndSaveWaitlist(
 ) {
 	await resend.emails.send({
 		from: 'Meen <onboarding@meen.co>',
-		to: ['markus@meen.co', 'hash@meen.co', 'lead-aaaap5b2bjt2ebl5vwhwgfuwne@meenai.slack.com'],
+		to: ['lead-aaaap5b2bjt2ebl5vwhwgfuwne@meenai.slack.com'],
 		subject: 'Meen - Waitlist',
 		html: `<p>New waitlist signup:</p>
 		<p>Email: ${email}</p>
@@ -25,7 +25,10 @@ async function notifyAndSaveWaitlist(
 		<p>Role: ${role}</p>`
 	});
 
-	await db.insert(waitlist).values({ email, name, company, companySize, role });
+	await db.insert(waitlist).values({ email, name, company, companySize, role }).onConflictDoUpdate({
+		target: waitlist.email,
+		set: { name, company, companySize, role }
+	});
 }
 export async function waitListEmail(
 	email: string,
@@ -41,6 +44,12 @@ export async function waitListEmail(
 		html: `<p>Welcome to the Meen waitlist! We'll notify you when we launch.</p>`
 	});
 
-	notifyAndSaveWaitlist(email, name, company || '', companySize || '', role || '');
+	notifyAndSaveWaitlist(
+		email,
+		name,
+		company || 'No Company Entered',
+		companySize || 'No Company Size Entered',
+		role || 'No Role Entered'
+	);
 	return data;
 }
--- src/routes/api/waitlist/+server.ts
+++ src/routes/api/waitlist/+server.ts
@@ -1,7 +1,7 @@
 import { waitListEmail } from '@/server/mail';
 
 export const POST = async ({ request }) => {
-	const { email, firstName, lastName, company, companySize, role } = await request.json();
-	await waitListEmail(email, firstName, lastName, company, companySize, role);
+	const { email, name, company, companySize, role } = await request.json();
+	await waitListEmail(email, name, company, companySize, role);
 	return new Response(JSON.stringify({ message: 'Email sent' }), { status: 200 });
 };
