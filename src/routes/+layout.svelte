5f04e1cce750554c4edaa609f6bfc0a1a3cb68c5
abbmarand <markusndrssn@gmail.com>
2025-04-23 11:59:52 +0200

    better grid

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -21,7 +21,7 @@
 	{@render children?.()}
 	{#if page.url.pathname === '/'}
 		<div
-			class="absolute inset-0 mt-[-2px] h-full bg-[linear-gradient(to_right,rgba(23,23,23,0.05)_1px,transparent_1px),linear-gradient(to_bottom,rgba(23,23,23,0.05)_1px,transparent_1px)] bg-[size:3rem_3rem] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_110%)] dark:bg-[linear-gradient(to_right,rgba(23,23,23,0.6)_1px,transparent_1px),linear-gradient(to_bottom,rgba(23,23,23,0.6)_1px,transparent_1px)]"
+			class="absolute inset-0 mt-[-2px] h-full bg-[linear-gradient(to_right,rgba(23,23,23,0.05)_1px,transparent_1px),linear-gradient(to_bottom,rgba(23,23,23,0.05)_1px,transparent_1px)] bg-[size:3rem_3rem] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_110%)] dark:bg-[linear-gradient(to_right,rgba(255,255,255,0.06)_1px,transparent_1px),linear-gradient(to_bottom,rgba(255,255,255,0.06)_1px,transparent_1px)]"
 		></div>
 	{/if}
 </div>
